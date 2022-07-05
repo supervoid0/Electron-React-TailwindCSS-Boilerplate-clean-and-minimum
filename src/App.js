f82f92c9063f6c86d6e16b3deadf76412786a30d
unknown <shabee.neil@gmail.com>
2022-07-05 09:55:52 +0900

    initialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-green-500">
+        ELECTRON REACT TAILWINDCSS BOILERPLATE
+      </div>
+    </div>
+  );
+}
+
+export default App;
